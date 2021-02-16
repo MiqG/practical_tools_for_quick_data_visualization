73b1062af3eabaad4be90e11bc305bb4febed0f4
MiqG <MiqG@github.com>
2021-02-17 00:12:41 +0100

    created unsolved exercises and intro; built webpage

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,11 @@
-# 
-# Author: Miquel Anglada Girotto
-# Contact: miquelangladagirotto [at] gmail [dot] com
-#
 # Script purpose
 # --------------
-# 
-#
-#
-# Outline
-# -------
+# Run whole project.
+
+set -euo pipefail
 
 # prepare raw data
-#Rscript scripts/prepare_multiomic_data.R
+Rscript scripts/prepare_multiomic_data.R
 
 # run report
 R -e "rmarkdown::render('reports/intro.Rmd', output_format='md_document')"
@@ -28,5 +22,8 @@ cp README.md book/
 
 # build book
 jupyter-book build book/
+
+# update webpage
+#ghp-import -n -p -f book/_build/html
 
 echo "Done!"
