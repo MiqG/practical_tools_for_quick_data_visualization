0dfd12fd2d7d5f80f0c88430d8542b7278839983
MiqG <MiqG@github.com>
2021-02-17 08:27:35 +0100

    restructuration and fixed typos

--- run_all.sh
+++ run_all.sh
@@ -8,14 +8,14 @@ set -euo pipefail
 Rscript scripts/prepare_multiomic_data.R
 
 # run report
-R -e "rmarkdown::render('reports/intro.Rmd', output_format='md_document')"
-R -e "rmarkdown::render('reports/exercise.Rmd', output_format='md_document')"
+R -e "rmarkdown::render('reports/a_solution/intro.Rmd', output_format='md_document')"
+R -e "rmarkdown::render('reports/a_solution/exercise.Rmd', output_format='md_document')"
 
 # move markdown and images
-mv reports/*.md book/
-cp -r reports/*_files book/
-rm -r reports/*_files
-cp -r reports/images book/
+mv reports/a_solution/*.md book/
+cp -r reports/a_solution/*_files book/
+rm -r reports/a_solution/*_files
+cp -r reports/a_solution/images book/
 
 # copy README
 cp README.md book/
@@ -23,7 +23,11 @@ cp README.md book/
 # build book
 jupyter-book build book/
 
+# update github
+git add .
+git commit -m "re-run whole project"
+
 # update webpage
-#ghp-import -n -p -f book/_build/html
+ghp-import -n -p -f book/_build/html
 
 echo "Done!"
